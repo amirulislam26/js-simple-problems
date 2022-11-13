fefdd56c39886f3ec2d01eabbf272c045985fc3f
amirulislam26 <26techamirulislam@gmail.com>
2022-11-13 07:17:46 -0800

    math js solving

---
+++ math.js
@@ -0,0 +1,14 @@
+const myNumber = -5;
+const outPut = Math.abs(myNumber);
+
+
+const myNumbers = 21.95;
+// const outPut = Math.ceil(myNumber);
+// const outPut = Math.floor(myNumber);
+// const outPut = Math.round(myNumber);
+
+for (let i = 0; i <= 30; i++){
+    const outPut = Math.random() * 6;
+    const rounded = Math.round(outPut);
+    console.log(rounded);
+}
